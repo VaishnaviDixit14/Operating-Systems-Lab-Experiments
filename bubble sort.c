928162715a5342be4bc6f3a2f25df0ac66f4ef34
Vaishnavi Dixit <107253182+VaishnaviDixit14@users.noreply.github.com>
2023-08-04 19:54:07 +0530

    Add files via upload

---
+++ bubble sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+//21BCE5126
+void bubble_sort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 8, 3, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i;
+
+    printf("Original array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    bubble_sort(arr, n);
+
+    printf("\nSorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
